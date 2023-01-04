5cfe2e3210b47ac04bd9afbea68f5f16f2f331ee
VGowthamTheja <vempalligowthamtheja@gmail.com>
2023-01-04 18:35:12 +0530

    Intial commit with React

---
+++ app/javascript/controllers/react_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from "../components/App"
+
+// Connects to data-controller="react"
+export default class extends Controller {
+  connect() {
+    console.log('React is Connected')
+
+    const app = document.getElementById('app');
+    createRoot(app).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    );
+  }
+}
